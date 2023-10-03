5cdda6084eec8faad89e8c32f103c6d3daede2bb
nahidul-fahim <nahidulfahim.official@gmail.com>
2023-10-03 20:19:05 +0600

    added forget password functionality

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -1,6 +1,8 @@
-import { useState } from "react";
-import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
+import { useRef, useState } from "react";
+import { getAuth, sendPasswordResetEmail, signInWithEmailAndPassword } from "firebase/auth";
 import app from "../../firebase.init";
+import { Link } from "react-router-dom";
+
 
 const Login = () => {
 
@@ -18,28 +20,58 @@ const Login = () => {
         setUserCheckFailed('');
 
         signInWithEmailAndPassword(auth, email, password)
-        .then(userDetails => {
-            const user = userDetails.user;
-            setUserCheck(user);
-        })
-        .catch(error => {
-            console.log(error.code)
-            console.log(error.message)
-            setUserCheckFailed(error.message);
-        })
+            .then(userDetails => {
+                const user = userDetails.user;
+                setUserCheck(user);
+            })
+            .catch(error => {
+                console.log(error.code)
+                console.log(error.message)
+                setUserCheckFailed(error.message);
+            })
     }
-
 
     const handleShowPassword = () => {
         setShowPassword(!showPassword);
+    };
+
+
+    const emailRef = useRef('');
+
+    const handleForgetPassword = () => {
+        console.log('forget password is working');
+        console.log(emailRef.current.value);
+        const emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+        if (!emailRef.current.value) {
+            alert("please provide a valid email first");
+            return;
+        }
+        else if (!emailPattern.test(emailRef.current.value)) {
+            alert("Please provide a valid email");
+            return;
+        }
+
+
+        sendPasswordResetEmail(auth, emailRef.current.value)
+            .then(
+                console.log("check your mail")
+            )
+            .catch(error => {
+                console.log(error.code)
+            })
+
     }
 
 
     return (
-        <div className="bg-[lightgray] p-10 rounded-md my-[100px] w-full lg:w-2/3 mx-auto flex flex-col justify-center items-center gap-y-10">
+        <div className="bg-[lightgray] p-10 rounded-md my-[100px] w-full lg:w-2/3 mx-auto flex flex-col justify-center items-center gap-y-10 relative">
+            <div className="absolute top-3 right-4 flex justify-center items-center gap-3">
+                <p className="font-medium text-[gray]"> New here?</p>
+                <Link className="font-semibold text-[#d12a2a] underline" to="/signup">Sign up</Link>
+            </div>
             <h2 className="text-3xl text-[#414141] font-bold">Login form</h2>
             <form onSubmit={handleLogin} className="w-full md:w-2/3 lg:w-[45%] space-y-8">
-                <input className="px-5 py-2 rounded-md border-[1px] border-[#ffffff00] focus:outline-none w-full focus:border-[1px] focus:border-[#acacac]" type="email" name="email" placeholder="Email address" id="email" required />
+                <input ref={emailRef} className="px-5 py-2 rounded-md border-[1px] border-[#ffffff00] focus:outline-none w-full focus:border-[1px] focus:border-[#acacac]" type="email" name="email" placeholder="Email address" id="email" required />
                 <div className="flex items-center">
                     <input className="px-5 py-2 rounded-md border-[1px] border-[#ffffff00] focus:outline-none w-full focus:border-[1px] focus:border-[#acacac]"
                         type={showPassword ? "text" : "password"}
@@ -49,7 +81,9 @@ const Login = () => {
                         required />
                     <span onClick={handleShowPassword} className="ml-[-50px] font-semibold text-[14px] cursor-pointer"><button>{showPassword ? 'hide' : 'show'}</button></span>
                 </div>
-                <input type="submit" value="Login" className="bg-[#2b2b2b] cursor-pointer text-white px-4 py-2 rounded-md font-semibold hover:bg-[gray] duration-300" />
+                <input type="submit" value="Login" className="bg-[#2b2b2b] cursor-pointer text-white px-4 py-2 rounded-md font-semibold hover:bg-[gray] duration-300 w-full" />
+
+                <input onClick={handleForgetPassword} type="submit" value="Forgot password?" className="bg-[#387add] cursor-pointer text-white px-4 py-2 rounded-md font-semibold hover:bg-[#20386b] duration-300 w-full" />
             </form>
             {
                 userCheckFailed ? <p className="text-base font-semibold text-[red]">Please check your email and password again</p> : ''
--- src/Components/SignUp/SignUp.jsx
+++ src/Components/SignUp/SignUp.jsx
@@ -1,6 +1,7 @@
-import { getAuth, createUserWithEmailAndPassword  } from "firebase/auth";
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
 import app from "../../firebase.init";
 import { useState } from "react";
+import { Link } from "react-router-dom";
 
 
 const SignUp = () => {
@@ -26,7 +27,7 @@ const SignUp = () => {
             setErrorMessage("Password should be 6 or more characters long");
             return;
         }
-        else if (!/[A-Z]/.test(password)){
+        else if (!/[A-Z]/.test(password)) {
             setErrorMessage("Your password should have at least one uppercase");
             return;
         }
@@ -37,14 +38,14 @@ const SignUp = () => {
 
         // Create user
         createUserWithEmailAndPassword(auth, email, password)
-        .then(userDetails => {
-            console.log(userDetails);
-            setSuccessMessage(userDetails)
-        })
-        .catch(error => {
-            console.error(error.message);
-            setErrorMessage(error.message)
-        })
+            .then(userDetails => {
+                console.log(userDetails);
+                setSuccessMessage(userDetails)
+            })
+            .catch(error => {
+                console.error(error.message);
+                setErrorMessage(error.message)
+            })
     }
 
     const handleShowPassword = () => {
@@ -54,19 +55,23 @@ const SignUp = () => {
 
 
     return (
-        <div className="bg-[lightgray] p-10 rounded-md my-[100px] w-full lg:w-2/3 mx-auto flex flex-col justify-center items-center gap-y-10">
+        <div className="bg-[lightgray] p-10 rounded-md my-[100px] w-full lg:w-2/3 mx-auto flex flex-col justify-center items-center gap-y-10 relative">
+            <div className="absolute top-3 right-4 flex justify-center items-center gap-3">
+                <p className="font-medium text-[gray]">Already have an account?</p>
+                <Link className="font-semibold text-[#d12a2a] underline" to="/login">Login</Link>
+            </div>
             <h2 className="text-3xl text-[#414141] font-bold">Signup form</h2>
             <form onSubmit={handleFormSubmission} className="w-full md:w-2/3 lg:w-[45%] space-y-8">
                 <input className="px-5 py-2 rounded-md border-[1px] border-[#ffffff00] focus:outline-none w-full focus:border-[1px] focus:border-[#acacac]" type="text" name="name" placeholder="Your name" id="name" />
                 <input className="px-5 py-2 rounded-md border-[1px] border-[#ffffff00] focus:outline-none w-full focus:border-[1px] focus:border-[#acacac]" type="email" name="email" placeholder="Email address" id="email" required />
                 <div className="flex items-center">
-                <input className="px-5 py-2 rounded-md border-[1px] border-[#ffffff00] focus:outline-none w-full focus:border-[1px] focus:border-[#acacac]"
-                type={showPassword ? "text" : "password"}
-                name="password"
-                placeholder="Password"
-                id="password"
-                required />
-                <span onClick={handleShowPassword} className="ml-[-50px] font-semibold text-[14px] cursor-pointer"><button>{showPassword ? 'hide' : 'show'}</button></span>
+                    <input className="px-5 py-2 rounded-md border-[1px] border-[#ffffff00] focus:outline-none w-full focus:border-[1px] focus:border-[#acacac]"
+                        type={showPassword ? "text" : "password"}
+                        name="password"
+                        placeholder="Password"
+                        id="password"
+                        required />
+                    <span onClick={handleShowPassword} className="ml-[-50px] font-semibold text-[14px] cursor-pointer"><button>{showPassword ? 'hide' : 'show'}</button></span>
                 </div>
                 <div className="flex justify-start items-center gap-x-2 font-medium">
                     <input type="checkbox" name="terms" id="terms" /><span>I agree to terms & conditions</span>
